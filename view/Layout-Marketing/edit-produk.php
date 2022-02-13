09aaebe7fb1b5c8935d31fcad4d189fbe2cc2031
mutaqin25 <mmutaqin1@gmail.com>
2022-02-13 13:17:34 +0700

    perubahan alert

--- view/Layout-Marketing/edit-produk.php
+++ view/Layout-Marketing/edit-produk.php
@@ -43,7 +43,8 @@ include '../../config.php';
 
     // cek apakah yang mengakses halaman ini sudah login
     if ($_SESSION['jenis_user'] !== "marketing") {
-        header("location:../../login.php?pesan=gagal");
+        $_SESSION['status'] = "gagal";
+        header("location:../../login.php");
     }
 
     ?>
@@ -136,10 +137,7 @@ include '../../config.php';
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
+
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
--- view/Layout-Marketing/edit-promosi.php
+++ view/Layout-Marketing/edit-promosi.php
@@ -42,7 +42,8 @@ include '../../config.php';
 
     // cek apakah yang mengakses halaman ini sudah login
     if ($_SESSION['jenis_user'] !== "marketing") {
-        header("location:../../login.php?pesan=gagal");
+        $_SESSION['status'] = "gagal";
+        header("location:../../login.php");
     }
 
     ?>
